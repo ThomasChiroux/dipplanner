ff88a2d580eb4f6e94319f8a57b02e86e164f77d
Thomas Chiroux <thomas@chiroux.com>
2012-05-06 17:43:29 +0200

    Other optims in compartment to reduce timing in the most used methods. Gain now > 100%

--- model/buhlmann/compartment.py
+++ model/buhlmann/compartment.py
@@ -63,9 +63,13 @@ class Compartment(object):
     self.pp_He = 0.0
     self.pp_N2 = 0.0
 
-    self.p_He_N2 = 0.0
     self.a_He_N2 = 0.0
     self.b_He_N2 = 0.0
+
+    self._const_exp_const_depth_He = None
+    self._const_exp_const_depth_N2 = None
+    self._old_kHe = None
+    self._old_seg_time = None
 
     if h_He is not None and h_N2 is not None and \
        a_He is not None and b_He is not None and \
@@ -102,7 +106,6 @@ class Compartment(object):
     
     Returns:
     <nothing>
-    
     """
     self.k_He = math.log(2) / (float(h_He)*60)
     self.k_N2 = math.log(2) / (float(h_N2)*60)
@@ -110,36 +113,34 @@ class Compartment(object):
     self.b_He = float(b_He)
     self.a_N2 = float(a_N2) / 10
     self.b_N2 = float(b_N2)
-    
+
   def set_pp(self, pp_He, pp_N2):
     """Sets partial pressures of He and N2
-    
+
     Keyword arguments:
     pp_He -- partial pressure of Helium
     pp_N2 -- partial pressure of Nitrogen
-    
+
     Return:
     <nothing>
-    
+
     Raise:
     ModelStateException -- if pp < 0
-    
-    """
-    if pp_He < 0.0 or pp_N2 < 0.0:
-      raise ModelStateException("Error in argument: negative pp is not allowed")
-    else:
-      self.pp_He = float(pp_He)
-      self.pp_N2 = float(pp_N2)
 
-      #calculate_p_a_b_inert
-      #Calculate and returns p_He_N2, a_He_N2 and b_He_N2
-      #based on current pp_He and pp_N2 of this compartment
-      self.p_He_N2 = self.pp_He + self.pp_N2
-      # calculate adjusted a, b coefficients based on those of He and N2
-      self.a_He_N2 = ((self.a_He * self.pp_He) + (self.a_N2 * self.pp_N2)) / self.p_He_N2
-      self.b_He_N2 = ((self.b_He * self.pp_He) + (self.b_N2 * self.pp_N2)) / self.p_He_N2
-      #self.p_He_N2, self.a_He_N2, self.b_He_N2 = self._calculate_p_a_b_inert()
-      
+    """
+    #if pp_He < 0.0 or pp_N2 < 0.0:
+    #  raise ModelStateException("Error in argument: negative pp is not allowed")
+    #else:
+    self.pp_He = pp_He
+    self.pp_N2 = pp_N2
+
+    #calculate_p_a_b_inert
+    #Calculate and returns a_He_N2 and b_He_N2
+    #based on current pp_He and pp_N2 of this compartment
+    # calculate adjusted a, b coefficients based on those of He and N2
+    self.a_He_N2 = ((self.a_He * pp_He) + (self.a_N2 * pp_N2)) / (pp_He + pp_N2)
+    self.b_He_N2 = ((self.b_He * pp_He) + (self.b_N2 * pp_N2)) / (pp_He + pp_N2)
+
   def const_depth(self, pp_He_inspired, pp_N2_inspired, seg_time):
     """Constant depth calculations. 
     Uses instananeous equation: P = Po + (Pi - Po)(1-e^-kt)
@@ -156,15 +157,36 @@ class Compartment(object):
     Raise:
     ModelStateException -- if pp or time < 0
     """
+    # below is an optimisation to reduce the (1 - math.exp(-self.k_He*float(seg_time)))
+    # calculation : only calculate when seg_time changes (k_He or k_N2 does
+    # not change for a dive)
+    if self._old_seg_time is None:
+      self._old_seg_time = seg_time
+      self._const_exp_const_depth_He = (1 - math.exp(-self.k_He*float(seg_time)))
+      self._const_exp_const_depth_N2 = (1 - math.exp(-self.k_N2*float(seg_time)))
+    elif self._old_seg_time != seg_time:
+      self._old_seg_time = seg_time
+      self._const_exp_const_depth_He = (1 - math.exp(-self.k_He*float(seg_time)))
+      self._const_exp_const_depth_N2 = (1 - math.exp(-self.k_N2*float(seg_time)))
+
     if pp_He_inspired < 0 or pp_N2_inspired < 0 or seg_time < 0:
       raise ModelStateException("Error in argument: negative value is not allowed")
     else:
-      new_pp_He = self.pp_He + ((pp_He_inspired - self.pp_He) * (1 - math.exp(-self.k_He*float(seg_time))))
-      new_pp_N2 = self.pp_N2 + ((pp_N2_inspired - self.pp_N2) * (1 - math.exp(-self.k_N2*float(seg_time))))
-      self.set_pp(new_pp_He, new_pp_N2)
-      #self.pp_He = new_pp_He
-      #self.pp_N2 = new_pp_N2
-      
+      new_pp_He = self.pp_He + ((pp_He_inspired - self.pp_He) * self._const_exp_const_depth_He)
+      new_pp_N2 = self.pp_N2 + ((pp_N2_inspired - self.pp_N2) * self._const_exp_const_depth_N2)
+      #self.set_pp(new_pp_He, new_pp_N2)
+
+      # below is an 'inline' version of set_pp for optimisation:
+      self.pp_He = new_pp_He
+      self.pp_N2 = new_pp_N2
+
+      #calculate_p_a_b_inert
+      #Calculate and returns a_He_N2 and b_He_N2
+      #based on current pp_He and pp_N2 of this compartment
+      # calculate adjusted a, b coefficients based on those of He and N2
+      self.a_He_N2 = ((self.a_He * new_pp_He) + (self.a_N2 * new_pp_N2)) / (new_pp_He + new_pp_N2)
+      self.b_He_N2 = ((self.b_He * new_pp_He) + (self.b_N2 * new_pp_N2)) / (new_pp_He + new_pp_N2)
+
   def asc_desc(self, pp_He_inspired, pp_N2_inspired, rate_he, rate_n2, seg_time):
       """Ascend or descent calculations.
       Uses equation : P=Pio+R(t -1/k)-[Pio-Po-(R/k)]e^-kt
@@ -188,11 +210,21 @@ class Compartment(object):
       else:
         new_pp_He = pp_He_inspired + rate_he * (float(seg_time) - (1.0/self.k_He)) - (pp_He_inspired - self.pp_He - (rate_he/self.k_He)) * math.exp(-self.k_He*float(seg_time))
         new_pp_N2 = pp_N2_inspired + rate_n2 * (float(seg_time) - (1.0/self.k_N2)) - (pp_N2_inspired - self.pp_N2 - (rate_n2/self.k_N2)) * math.exp(-self.k_N2*float(seg_time))
-        self.set_pp(new_pp_He, new_pp_N2)
-        #self.pp_He = new_pp_He
-        #self.pp_N2 = new_pp_N2
-  
-#  def _calculate_p_a_b_inert(self):
+        #self.set_pp(new_pp_He, new_pp_N2)
+
+        # below is an 'inline' version of set_pp for optimisation:
+        self.pp_He = new_pp_He
+        self.pp_N2 = new_pp_N2
+
+        #calculate_p_a_b_inert
+        #Calculate and returns a_He_N2 and b_He_N2
+        #based on current pp_He and pp_N2 of this compartment
+        # calculate adjusted a, b coefficients based on those of He and N2
+        self.a_He_N2 = ((self.a_He * new_pp_He) + (self.a_N2 * new_pp_N2)) / (new_pp_He + new_pp_N2)
+        self.b_He_N2 = ((self.b_He * new_pp_He) + (self.b_N2 * new_pp_N2)) / (new_pp_He + new_pp_N2)
+
+
+  #  def _calculate_p_a_b_inert(self):
 #    """Calculate and returns p_He_N2, a_He_N2 and b_He_N2
 #    based on current pp_He and pp_N2 of this compartment
 #
@@ -223,10 +255,8 @@ class Compartment(object):
     float, M_value : maximum tolerated pressure in bar
     
     """
-    #p_He_N2, a_He_N2, b_He_N2 = self._calculate_p_a_b_inert()
     return float(pressure) / self.b_He_N2 + self.a_He_N2
-    #return mv
-    
+
   def get_max_amb(self, gf):
     """Gets Tolerated Absolute Pressure for the compartment
     for current pp of He and N2
@@ -238,12 +268,8 @@ class Compartment(object):
     float, maximum tolerated pressure in bar
     
     """
-    #p_He_N2, a_He_N2, b_He_N2 = self._calculate_p_a_b_inert()
+    return ((self.pp_He + self.pp_N2) - self.a_He_N2 * gf) / (gf / self.b_He_N2 - gf + 1.0)
 
-    #max_amb = (p_He_N2 - a_He_N2 ) /  b_He_N2 
-    return (self.p_He_N2 - self.a_He_N2 * gf) / (gf / self.b_He_N2 - gf + 1.0)
-    #return max_amb
-    
   def get_mv(self, p_amb):
     """Gets M-Value for a compartment, given an ambient pressure
     
@@ -254,7 +280,5 @@ class Compartment(object):
     float, M-value
     
     """
-    #p_He_N2, a_He_N2, b_He_N2 = self._calculate_p_a_b_inert()
-    #mv = float(p_amb) / b_He_N2 + a_He_N2
     #self.logger.debug("comp m-value for %s : %s" % (p_amb, mv))
-    return self.p_He_N2 / (float(p_amb) / self.b_He_N2 + self.a_He_N2)
+    return (self.pp_He + self.pp_N2) / (float(p_amb) / self.b_He_N2 + self.a_He_N2)
